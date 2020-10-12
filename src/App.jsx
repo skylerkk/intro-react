fb7706cd8459c5293d3998e3ec028d27c42bd290
skylerkk <kuretza.skyler@gmail.com>
2020-10-12 14:42:19 -0400

    uncomment grades section

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ class App extends React.Component {
     }
 
     /* Uncomment lines below to render grades*/
-    /*if (this.state.buttonClicked === "grades") {
+    if (this.state.buttonClicked === "grades") {
       tabChoice = (
         <Table
           tableNames={this.state.assignments}
@@ -89,8 +89,8 @@ class App extends React.Component {
           data={this.state.grades}
         />
       );
-    }*/
-
+    }
+    
     return (
       <div>
         <div className="Box Box--spacious f4">
